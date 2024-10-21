b25d59af40303443c15f3e5dc1c3cd8366a9beed
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2024-10-21 06:08:45 +0100

    Add session authentication to the DEFAULT_AUTHENTICATION_CLASSES for browsable API

--- config/settings.py
+++ config/settings.py
@@ -145,6 +145,7 @@ urlpatterns = [
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
     ),
 }
 

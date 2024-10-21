166ff3c206944b91da071be1607bed3b096797ce
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2024-10-21 05:11:44 +0100

    Configure urls.py to serve user-uploaded media files during development

--- config/settings.py
+++ config/settings.py
@@ -130,3 +130,10 @@ AUTH_USER_MODEL = 'users.CustomUser'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    # ... 
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

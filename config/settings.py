eb03db590ea5d4dc25aaa562f2230334f8679f19
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2024-10-21 15:38:02 +0100

    Refactor debug mode to False in settings.py

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y180vv=v(@=tjz%0wk#wnmfdi0ou3fa8imdf%7^(o8--@^5__!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 

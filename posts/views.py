51da8d014d9afab2ec01b274a4fcab80f0d24f56
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2024-10-21 06:03:49 +0100

    Refactor views.py in posts app to add serializers and permission classes

--- posts/views.py
+++ posts/views.py
@@ -45,3 +45,50 @@ class PostViewSet(viewsets.ModelViewSet):
             self.permission_classes = (permissions.AllowAny,)
 
         return super().get_permissions()
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """
+    CRUD comments for a particular post
+    """
+
+    queryset = Comment.objects.all()
+
+    def get_queryset(self):
+        res = super().get_queryset()
+        post_id = self.kwargs.get("post_id")
+        return res.filter(post__id=post_id)
+
+    def get_serializer_class(self):
+        if self.action in ("create", "update", "partial_update", "destroy"):
+            return CommentWriteSerializer
+
+        return CommentReadSerializer
+
+    def get_permissions(self):
+        if self.action in ("create",):
+            self.permission_classes = (permissions.IsAuthenticated,)
+        elif self.action in ("update", "partial_update", "destroy"):
+            self.permission_classes = (IsAuthorOrReadOnly,)
+        else:
+            self.permission_classes = (permissions.AllowAny,)
+
+        return super().get_permissions()
+
+class LikePostAPIView(APIView):
+    """
+    Like, Dislike a post
+    """
+
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get(self, request, pk):
+        user = request.user
+        post = get_object_or_404(Post, pk=pk)
+
+        if user in post.likes.all():
+            post.likes.remove(user)
+
+        else:
+            post.likes.add(user)
+
+        return Response(status=status.HTTP_200_OK)

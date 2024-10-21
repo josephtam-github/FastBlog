85388bc7764da73fc0acecc82eb1cd17ac8b3159
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2024-10-21 06:00:48 +0100

    Refactor views.py in posts app to add serializers and permission classes

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import permissions, status, viewsets
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from posts.models import Category, Comment, Post
+from posts.serializers import (
+    CategoryReadSerializer,
+    CommentReadSerializer,
+    CommentWriteSerializer,
+    PostReadSerializer,
+    PostWriteSerializer,
+)
+
+from .permissions import IsAuthorOrReadOnly
+
+class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    List and Retrieve post categories
+    """
+
+    queryset = Category.objects.all()
+    serializer_class = CategoryReadSerializer
+    permission_classes = (permissions.AllowAny,)
+
+class PostViewSet(viewsets.ModelViewSet):
+    """
+    CRUD posts
+    """
+
+    queryset = Post.objects.all()
+
+    def get_serializer_class(self):
+        if self.action in ("create", "update", "partial_update", "destroy"):
+            return PostWriteSerializer
+
+        return PostReadSerializer
+
+    def get_permissions(self):
+        if self.action in ("create",):
+            self.permission_classes = (permissions.IsAuthenticated,)
+        elif self.action in ("update", "partial_update", "destroy"):
+            self.permission_classes = (IsAuthorOrReadOnly,)
+        else:
+            self.permission_classes = (permissions.AllowAny,)
+
+        return super().get_permissions()

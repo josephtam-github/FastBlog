28d878850deb8640b66186f9c0ca10e3cd809e6b
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2024-10-21 05:44:45 +0100

    Register Category, Post, and Comment models in admin site

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,8 @@
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Comment, Post
+
+admin.site.register(Category)
+admin.site.register(Post)
+admin.site.register(Comment)

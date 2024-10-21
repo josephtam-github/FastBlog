13b19b6e7ddf696b079be83cf26e00e9181aa09b
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2024-10-21 05:35:03 +0100

    Add serializers and views for user registration, login, and profile endpoints

--- users/views.py
+++ users/views.py
@@ -44,3 +44,55 @@ class UserLoginAPIView(GenericAPIView):
         data = serializer.data
         data["tokens"] = {"refresh": str(token), "access": str(token.access_token)}
         return Response(data, status=status.HTTP_200_OK)
+
+
+class UserLogoutAPIView(GenericAPIView):
+    """
+    An endpoint to logout users.
+    """
+
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request, *args, **kwargs):
+        try:
+            refresh_token = request.data["refresh"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+class UserAPIView(RetrieveUpdateAPIView):
+    """
+    Get, Update user information
+    """
+
+    permission_classes = (IsAuthenticated,)
+    serializer_class = serializers.CustomUserSerializer
+
+    def get_object(self):
+        return self.request.user
+
+class UserProfileAPIView(RetrieveUpdateAPIView):
+    """
+    Get, Update user profile
+    """
+
+    queryset = Profile.objects.all()
+    serializer_class = serializers.ProfileSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user.profile
+
+class UserAvatarAPIView(RetrieveUpdateAPIView):
+    """
+    Get, Update user avatar
+    """
+
+    queryset = Profile.objects.all()
+    serializer_class = serializers.ProfileAvatarSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user.profile

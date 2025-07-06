62d7020c6819f89181b3286d840c515077bb0dc1
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2025-07-06 18:59:22 +0100

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -86,10 +86,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='postgresql://fastblog:fastblog@localhost:5432/mysite',
-        conn_max_age=600    
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # Password validation
